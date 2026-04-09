38cb040f5b9b1568ced162ec00b3ea776b65deff
Dineth Hirusha <dineth.hirusha.pro@gmail.com>
2026-04-09 16:06:52 +0530

    impl(enum): Create an Enum for Authentication Providers

---
+++ backend/src/main/java/com/nelli/smart_campus_operations_hub/enums/AuthProvider.java
@@ -0,0 +1,9 @@
+package com.nelli.smart_campus_operations_hub.enums;
+
+/**
+ * Identifies the authentication source used for a user account.
+ */
+public enum AuthProvider {
+    LOCAL,
+    GOOGLE
+}
