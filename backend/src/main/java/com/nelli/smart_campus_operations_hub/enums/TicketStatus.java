669f82dc514d3052f8294fe8a4fa560257878ec7
Dineth Hirusha <dineth.hirusha.pro@gmail.com>
2026-04-16 18:27:26 +0530

    package com.nelli.smart_campus_operations_hub.enums;

---
+++ backend/src/main/java/com/nelli/smart_campus_operations_hub/enums/TicketStatus.java
@@ -0,0 +1,12 @@
+package com.nelli.smart_campus_operations_hub.enums;
+
+/**
+ * Represents the workflow states of a maintenance ticket.
+ */
+public enum TicketStatus {
+    OPEN,
+    IN_PROGRESS,
+    RESOLVED,
+    CLOSED,
+    REJECTED
+}
