f5c57b2ea662fdfa2bfff216ebfa088ac708b812
Dineth Hirusha <dineth.hirusha.pro@gmail.com>
2026-04-16 18:28:21 +0530

    impl(enum): Created TicketStatus, TicketCategory, and TicketPriority enums

--- backend/src/main/java/com/nelli/smart_campus_operations_hub/enums/TicketStatus.java
+++ backend/src/main/java/com/nelli/smart_campus_operations_hub/enums/TicketStatus.java
@@ -1,7 +1,7 @@
 package com.nelli.smart_campus_operations_hub.enums;
 
 /**
- * Represents the workflow states of a maintenance ticket.
+ * Represents the workflow states of a maintenance tickets.
  */
 public enum TicketStatus {
     OPEN,
