c04d0e41143f835073ee92477c80a48ef65e15d1
Dineth Hirusha <dineth.hirusha.pro@gmail.com>
2026-04-19 19:14:25 +0530

    impl(exception): Created BookingConflictException for overlap detection

--- backend/src/main/java/com/nelli/smart_campus_operations_hub/exception/BookingConflictException.java
+++ backend/src/main/java/com/nelli/smart_campus_operations_hub/exception/BookingConflictException.java
@@ -1,4 +1,27 @@
 package com.nelli.smart_campus_operations_hub.exception;
 
-public class BookingConflictException {
+import com.nelli.smart_campus_operations_hub.model.Booking;
+import java.util.List;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Thrown when a booking request conflicts with existing bookings.
+ */
+@ResponseStatus(HttpStatus.CONFLICT)
+@Getter
+public class BookingConflictException extends RuntimeException {
+
+    private final List<Booking> conflictingBookings;
+
+    public BookingConflictException(String message) {
+        super(message);
+        this.conflictingBookings = null;
+    }
+
+    public BookingConflictException(String message, List<Booking> conflictingBookings) {
+        super(message);
+        this.conflictingBookings = conflictingBookings;
+    }
 }
